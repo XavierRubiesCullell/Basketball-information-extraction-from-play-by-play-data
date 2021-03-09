d144ba750ebc79afa39133dee78c3c77f3a09597
XavierRubiesCullell <xrubiescullell@hotmail.com>
2021-03-09 04:57:52 +0100

    Partial boxscore adjustment

--- Boxscore automàtic/Main.py
+++ Boxscore automàtic/Main.py
@@ -2,8 +2,8 @@ from StandardPbPObtention import StandardPbPObtentionMain
 from BoxscoreObtention import BoxscoreObtentionMain
 
 date = '20210107'
-home = 'Portland'
-visiting = 'Minnesota'
+home = 'Memphis'
+visiting = 'Cavaliers'
 
 StandardPbPObtentionMain('https://www.basketball-reference.com/boxscores/pbp/'+date+'0'+home[:3].upper()+'.html', out_file = home+visiting+date+"_StandardPbP.txt")
-BoxscoreObtentionMain(in_file = home+visiting+date+"_StandardPbP.txt", pkl1 = home+visiting+date+"_BS_"+home+".pkl", pkl2 = home+visiting+date+"_BS_"+visiting+".pkl")
+BoxscoreObtentionMain(in_file = home+visiting+date+"_StandardPbP.txt", pkl1 = home+visiting+date+"_BS_"+home+".pkl", pkl2 = home+visiting+date+"_BS_"+visiting+".pkl", start="48:00", end="36:00")
